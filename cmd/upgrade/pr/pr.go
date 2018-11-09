ee54e1a6f19bd7a1512791421d91b825c2a62233
Alex Pilon <apilon@hashicorp.com>
2018-11-09 16:48:59 -0500

    implement pull request

--- cmd/upgrade/pr/pr.go
+++ cmd/upgrade/pr/pr.go
@@ -35,20 +35,25 @@ func CommandFactory() (cli.Command, error) {
 
 func (c *command) Run(args []string) int {
 	flags := flag.NewFlagSet(CommandName, flag.ExitOnError)
-	var err error
 	var branch string
 	var message string
 	var remote string
 	var open bool
-	var providerPath string
+	var base string
+	var user string
+	var title string
+	var provider string
 	flags.StringVar(&branch, "branch", "tfplugin-upgrade-"+time.Now().Format("2006-01-02-15-04-05"), "name of branch to create")
 	flags.StringVar(&message, "message", "sdk upgrade", "commit message")
 	flags.StringVar(&remote, "remote", "origin", "remote to push to")
-	flags.StringVar(&providerPath, "provider", "", "provider to PR")
-	flags.BoolVar(&open, "open", false, "Optionally open PR automatically, requires GITHUB_PERSONAL_TOKEN")
+	flags.StringVar(&provider, "provider", "", "provider to pull request")
+	flags.StringVar(&base, "base", "master", "base branch to open pull request against")
+	flags.StringVar(&user, "user", "", "github user/org for cross account pull requests")
+	flags.StringVar(&title, "title", "sdk upgrade", "title of the pull request")
+	flags.BoolVar(&open, "open", false, "open pull request automatically, requires GITHUB_PERSONAL_TOKEN")
 	flags.Parse(args)
 
-	providerPath, err = cmd.FindProvider(providerPath)
+	providerPath, err := cmd.FindProvider(provider)
 	if err != nil {
 		log.Printf("Error finding provider: %s", err)
 		return 1
@@ -75,7 +80,7 @@ func (c *command) Run(args []string) int {
 	}
 
 	if open {
-		if err = openPullRequest(providerPath, remote, branch); err != nil {
+		if err = openPullRequest(providerPath, base, branch, user, title); err != nil {
 			log.Printf("Error opening pull request: %s", err)
 			return 1
 		}
@@ -93,7 +98,7 @@ func run(dir, name string, arg ...string) error {
 	return cmd.Run()
 }
 
-func openPullRequest(providerPath, remote, branch string) error {
+func openPullRequest(providerPath, base, head, user, title string) error {
 	token := os.Getenv("GITHUB_PERSONAL_TOKEN")
 	if token == "" {
 		return errors.New("No GITHUB_PERSONAL_TOKEN set")
@@ -104,8 +109,34 @@ func openPullRequest(providerPath, remote, branch string) error {
 		&oauth2.Token{AccessToken: token},
 	)
 	tc := oauth2.NewClient(ctx, ts)
-
 	client := github.NewClient(tc)
 
+	owner, repo, err := getGitHubDetails(providerPath)
+	if err != nil {
+		return err
+	}
+
+	if user != "" {
+		head = user + ":" + head
+	}
+
+	pr, _, err := client.PullRequests.Create(ctx, owner, repo, &github.NewPullRequest{
+		Title: github.String(title),
+		Head:  github.String(head),
+		Base:  github.String(base),
+	})
+	if err != nil {
+		return err
+	}
+	fmt.Printf("\nPull request created, view at: %s\n", pr.GetHTMLURL())
 	return nil
+}
+
+func getGitHubDetails(providerPath string) (string, string, error) {
+	// format is .../owner/repo
+	parts := strings.Split(providerPath, "/")
+	if len(parts) < 2 {
+		return "", "", fmt.Errorf("%s should follow '.../owner/repo' format", providerPath)
+	}
+	return parts[len(parts)-2], parts[len(parts)-1], nil
 }
