35d4deba62b3d69989e7ae5b811c2ba08b358c40
Alex Pilon <apilon@hashicorp.com>
2018-11-09 16:00:15 -0500

    vendor

--- cmd/upgrade/pr/pr.go
+++ cmd/upgrade/pr/pr.go
@@ -1,6 +1,8 @@
 package pr
 
 import (
+	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -10,7 +12,9 @@ import (
 	"time"
 
 	"github.com/appilon/tfplugin/cmd"
+	"github.com/google/go-github/github"
 	"github.com/mitchellh/cli"
+	"golang.org/x/oauth2"
 )
 
 const CommandName = "upgrade pr"
@@ -35,11 +39,13 @@ func (c *command) Run(args []string) int {
 	var branch string
 	var message string
 	var remote string
+	var open bool
 	var providerPath string
 	flags.StringVar(&branch, "branch", "tfplugin-upgrade-"+time.Now().Format("2006-01-02-15-04-05"), "name of branch to create")
 	flags.StringVar(&message, "message", "sdk upgrade", "commit message")
 	flags.StringVar(&remote, "remote", "origin", "remote to push to")
 	flags.StringVar(&providerPath, "provider", "", "provider to PR")
+	flags.BoolVar(&open, "open", false, "Optionally open PR automatically, requires GITHUB_PERSONAL_TOKEN")
 	flags.Parse(args)
 
 	providerPath, err = cmd.FindProvider(providerPath)
@@ -68,9 +74,11 @@ func (c *command) Run(args []string) int {
 		return 1
 	}
 
-	if err = openPullRequest(providerPath, remote, branch); err != nil {
-		log.Printf("Error opening pull request: %s", err)
-		return 1
+	if open {
+		if err = openPullRequest(providerPath, remote, branch); err != nil {
+			log.Printf("Error opening pull request: %s", err)
+			return 1
+		}
 	}
 
 	return 0
@@ -86,5 +94,18 @@ func run(dir, name string, arg ...string) error {
 }
 
 func openPullRequest(providerPath, remote, branch string) error {
+	token := os.Getenv("GITHUB_PERSONAL_TOKEN")
+	if token == "" {
+		return errors.New("No GITHUB_PERSONAL_TOKEN set")
+	}
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
 	return nil
 }
