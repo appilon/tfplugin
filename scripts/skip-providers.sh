84f03cb08f3763532a1c261931d9ff12101bcb4c
Alex Pilon <apilon@hashicorp.com>
2019-01-07 16:50:11 -0500

    fix bash

--- scripts/skip-providers.sh
+++ scripts/skip-providers.sh
@@ -1,11 +1,11 @@
 skip_provider () {
     # declare array of providers to skip
-    declare -a arr=("aws oci azurerm google google-beta kubernetes")
+    declare -a arr=("aws" "oci" "azurerm" "google" "google-beta" "kubernetes")
     for i in "${arr[@]}"
     do
         if [ "terraform-provider-$i" = "$1" ]
         then
-            echo "skip"
+            echo "skip $1"
             return
         fi
     done
