08aa12c11d82dacbd151b1b8958e77e7f6300e5c
Alex Pilon <apilon@hashicorp.com>
2019-02-20 18:08:15 -0500

    tweak scripts

--- scripts/switch-ready-providers-to-modules.sh
+++ scripts/switch-ready-providers-to-modules.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 scripts=$(dirname "$0")
 
-tfplugin status -ready-for-modules | xargs $scripts/switch-to-modules.sh
+tfplugin status -ready-for-modules | xargs -L 1 $scripts/switch-to-modules.sh
--- scripts/switch-to-modules.sh
+++ scripts/switch-to-modules.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
+
 set -Eeuo pipefail
+
+scripts=$(dirname "$0")
+source "$scripts/skip-providers.sh"
+
+repo_dir=$(basename $1)
+skip=$(skip_provider "$repo_dir")
+if [[ ! -z "$skip" ]]
+then
+    echo "skipping...$skip"
+    exit 0
+fi
 
 pushd $GOPATH/src/$1
 
 git checkout -f master
 git pull
+git branch -D "go-modules-$(date +%F)"
 git checkout -b "go-modules-$(date +%F)"
 # this is mainly to encode the .go-version, will create commit message
 # stating it did a few things that are likely noop
@@ -12,7 +25,7 @@ git checkout -b "go-modules-$(date +%F)"
 tfplugin upgrade go -to="1.11.5" -fix -fmt -encode -commit
 tfplugin upgrade modules -commit
 # upgrade to v0.11.12 which has a clean transitive dependency story that uses go modules
-tfplugin ugprade sdk -to=sdk-v0.11-with-go-modules -commit
+tfplugin upgrade sdk -to=sdk-v0.11-with-go-modules -commit
 tfplugin upgrade pr -branch="$(git rev-parse --abbrev-ref HEAD)" -title="[MODULES] Switch to Go Modules"
 
 popd
