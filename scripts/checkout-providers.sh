fc3bab9a3cc7df100299b4092e093e65d1e5dd69
Alex Pilon <apilon@hashicorp.com>
2019-03-11 20:05:22 -0400

    grab page 2 of providers

--- scripts/checkout-providers.sh
+++ scripts/checkout-providers.sh
@@ -7,9 +7,19 @@ source "$scripts/skip-providers.sh"
 mkdir -p $GOPATH/src/github.com/terraform-providers
 pushd $GOPATH/src/github.com/terraform-providers
 
-curl 'https://api.github.com/orgs/terraform-providers/repos?per_page=200' > repos.json
+curl 'https://api.github.com/orgs/terraform-providers/repos?per_page=100' > repos.json
+curl 'https://api.github.com/orgs/terraform-providers/repos?per_page=100&page=2' > repos2.json
 
 for git_uri in $(cat repos.json | jq -r '.[] | select( .archived == false ) | .ssh_url'); do
+    repo_dir=$(basename $git_uri .git)
+    skip=$(skip_provider "$repo_dir")
+    if [[ ! -d $repo_dir ]] && [[ -z "$skip" ]]
+    then
+        git clone --depth 1 --no-single-branch $git_uri
+    fi
+done
+
+for git_uri in $(cat repos2.json | jq -r '.[] | select( .archived == false ) | .ssh_url'); do
     repo_dir=$(basename $git_uri .git)
     skip=$(skip_provider "$repo_dir")
     if [[ ! -d $repo_dir ]] && [[ -z "$skip" ]]
