6107865ddc6923f4369836ee72defd3ec48a9035
Alex Pilon <apilon@hashicorp.com>
2019-03-11 21:27:54 -0400

    tweak checkout script

--- scripts/checkout-providers.sh
+++ scripts/checkout-providers.sh
@@ -7,11 +7,12 @@ source "$scripts/skip-providers.sh"
 mkdir -p $GOPATH/src/github.com/terraform-providers
 pushd $GOPATH/src/github.com/terraform-providers
 
-curl 'https://api.github.com/orgs/terraform-providers/repos?per_page=100' > repos1.json
-curl 'https://api.github.com/orgs/terraform-providers/repos?per_page=100&page=2' > repos2.json
-jq -s '[.[][]]' *.json > repos.json
-rm repos1.json
-rm repos2.json
+rm repos.json
+curl 'https://api.github.com/orgs/terraform-providers/repos?per_page=100&page=1' > repos-page1.json
+curl 'https://api.github.com/orgs/terraform-providers/repos?per_page=100&page=2' > repos-page2.json
+jq -s '[.[][]]' repos-page*.json > repos.json
+rm repos-page1.json
+rm repos-page2.json
 
 for git_uri in $(cat repos.json | jq -r '.[] | select( .archived == false ) | .ssh_url'); do
     repo_dir=$(basename $git_uri .git)
